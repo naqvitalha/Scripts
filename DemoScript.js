432559fb691d9c2abf54057abd4f499566f26bfc
naqvitalha <naqvitalha@gmail.com>
2017-11-30 10:39:49 +0530

    Update

--- DemoScript.js
+++ DemoScript.js
@@ -1,21 +1,26 @@
-var fileref=document.createElement('script');
-fileref.setAttribute("type","text/javascript");
-fileref.setAttribute("src", "http://yourjavascript.com/101317231712/fkext-browser-min.js");
-var head = document.getElementsByTagName('head')[0];
-head.insertBefore(fileRef, head.firstChild);  
+var fileref = document.createElement("script");
+fileref.setAttribute("type", "text/javascript");
+fileref.setAttribute("src", "https://cdn.rawgit.com/naqvitalha/Scripts/master/fkext-browser-min.js");
+var head = document.getElementsByTagName("head")[0];
+head.insertBefore(fileref, head.firstChild);
 
-
-var doOnce = false;
+var platform;
+var moduleManager;
 setInterval(function() {
-    if (window.location.href.startsWith("https://www.makemytrip.com/pwa/flight-traveller-details")) {
-        var item = document.getElementsByClassName("continue_btn");
-        if (item.length > 0) {
-            item[0].onclick = function() {
-                alert("Hello");
-            };
+    if (platform) {
+        if (window.location.href.startsWith("https://www.makemytrip.com/pwa/flight-traveller-details")) {
+            var item = document.getElementsByClassName("continue_btn");
+            if (item.length > 0) {
+                item[0].onclick = function() {
+                    moduleManager.getNavigationModule().startPayment("PN00171130103626313a84c6429d458fe8ce7d96e9600154_v1_UNCRN");
+                };
+            }
         }
-    }
-    if(window.location.href === "https://www.makemytrip.com/"){
-
+    } else {
+        if (window.FKExtension) {
+            platform = FKExtension.newPlatformInstance("mmt");
+            moduleManager = platform.getModuleHelper();
+            moduleManager.getPermissionsModule().getToken(["user.email"]);
+        }
     }
 }, 500);
